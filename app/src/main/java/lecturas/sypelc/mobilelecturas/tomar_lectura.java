65a2a0de75c3735ec21fdc844c11c36e7f3043ba
mafesitaotalora <fernanda.otalora@sypelcltda.com>
2015-02-02 08:19:11 -0500

    Layout
    Se corrigen los layout.

--- app/src/main/java/lecturas/sypelc/mobilelecturas/tomar_lectura.java
+++ app/src/main/java/lecturas/sypelc/mobilelecturas/tomar_lectura.java
@@ -8,6 +8,7 @@ import android.view.MenuItem;
 
 public class tomar_lectura extends ActionBarActivity {
 
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
