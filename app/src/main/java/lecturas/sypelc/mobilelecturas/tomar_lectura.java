836f7f82425e55082e9f7cc513ed0e03c81a7c3f
JulianPoveda <poveda_daza@hotmail.com>
2015-02-03 19:12:03 -0500

    Modificacion Form TomaLectura
    Se realiza la modificacion del formulario TomaLectura con el fin de que
    la informacion quede mejor distribuida, se crea el view flipper para
    poder cambiar de usuarios deslizando los dedos de izquierda a derecha y
    viceversa, queda pendiente las funcionalidades detalladas.

--- app/src/main/java/lecturas/sypelc/mobilelecturas/tomar_lectura.java
+++ app/src/main/java/lecturas/sypelc/mobilelecturas/tomar_lectura.java
@@ -4,15 +4,46 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.ViewFlipper;
 
 
-public class tomar_lectura extends ActionBarActivity {
+public class tomar_lectura extends ActionBarActivity implements View.OnTouchListener{
+
+    private ViewFlipper _viewFlipper;
+    private TextView    _lblCuenta, _lblNombre, _lblDireccion, _lblRuta, _lblMedidor, _lblTipoUso;
+    private TextView    _lblAnomalia, _lblLectura1, _lblLectura2, _lblLectura3, _lblMensaje;
+    private EditText    _txtLectura1, _txtLectura2, _txtLectura3, _txtMensaje;
+
+    private Spinner     _cmbTipoUso, _cmbAnomalia;
+
 
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tomar_lectura);
+
+        this._lblCuenta     = (TextView) findViewById(R.id.LecturaTxtCuenta);
+        this._lblNombre     = (TextView) findViewById(R.id.LecturaTxtNombre);
+        this._lblDireccion  = (TextView) findViewById(R.id.LecturaTxtDireccion);
+        this._lblRuta       = (TextView) findViewById(R.id.LecturaTxtRuta);
+        this._lblMedidor    = (TextView) findViewById(R.id.LecturaTxtMedidor);
+
+        this._txtLectura1   = (EditText) findViewById(R.id.LecturaEditLectura1);
+        this._txtLectura2   = (EditText) findViewById(R.id.LecturaEditLectura2);
+        this._txtLectura3   = (EditText) findViewById(R.id.LecturaEditLectura3);
+        this._txtMensaje    = (EditText) findViewById(R.id.LecturaEditMensaje);
+
+        this._cmbAnomalia   = (Spinner) findViewById(R.id.LecturaSpnAnomalia);
+        this._cmbTipoUso    = (Spinner) findViewById(R.id.LecturaSpnTipoUso);
+
+        this._viewFlipper = (ViewFlipper) findViewById(R.id.InicioViewFlipper);
+        this._viewFlipper.setOnTouchListener(this);
     }
 
 
@@ -37,4 +68,28 @@ public class tomar_lectura extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN: //Cuando el usuario toca la pantalla por primera vez
+                //init_x=event.getX();
+                return true;
+            case MotionEvent.ACTION_UP: //Cuando el usuario deja de presionar
+                //float distance =init_x-event.getX();
+
+                /*if(distance>0){
+                    _viewFlipper.showPrevious();
+                }
+
+                if(distance<0){
+                    _viewFlipper.showNext();
+                }*/
+
+            default:
+                break;
+        }
+        return false;
+    }
+
 }
