20521e0319d46aa991885e4a0a609d75767dae32
mafesitaotalora <fernanda.otalora@sypelcltda.com>
2015-02-06 11:10:03 -0500

    Classe Anomlia
    se corrige para devolver array string en las funciones listaranomalias,
    listardatos

--- app/src/main/java/clases/ClassAnomalia.java
+++ app/src/main/java/clases/ClassAnomalia.java
@@ -3,6 +3,9 @@ package clases;
 import android.content.ContentValues;
 import android.content.Context;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 import lecturas.sypelc.mobilelecturas.FormInicioSession;
 import sistema.SQLite;
 
@@ -15,18 +18,19 @@ public class ClassAnomalia {
 
     private SQLite FcnSQL;
 
-    private String tipo_uso;
     private String anomalia;
+    private ArrayList<String> listadoAnomalias;
+    private ArrayList<String> listarDatos;
 
     public ClassAnomalia(Context _ctx){
         this.context = _ctx;
         this.FcnSQL         = new SQLite(this.context, FormInicioSession.path_files_app);
+        listadoAnomalias = new ArrayList<String>();
     }
 
-    public ContentValues listarAnomalias(String _tipo_uso) {
+    public ArrayList<String> listarAnomalias(String _tipo_uso) {
         this._tempRegistro.clear();
-        this.tipo_uso = _tipo_uso;
-        if(this.tipo_uso == "Residencial"){
+        if(_tipo_uso.equals("RS")){
             this._tempRegistro =    this.FcnSQL.SelectDataRegistro( "param_anomalias",
                                                                     "id_anomalia, descripcion",
                                                                     "aplica_residencial='t'");
@@ -36,15 +40,20 @@ public class ClassAnomalia {
                                                                     "id_anomalia, descripcion",
                                                                     "aplica_no_residencial='t'");
         }
-        return _tempRegistro;
+        listadoAnomalias.add(this._tempRegistro.getAsString("id_anomalia"));
+        listadoAnomalias.add(this._tempRegistro.getAsString("descripcion"));
+        return listadoAnomalias;
     }
 
-    public ContentValues validarDatosAnomalia(String _anomalia){
+    public ArrayList<String> validarDatosAnomalia(String _anomalia){
         this._tempRegistro.clear();
         this.anomalia = _anomalia;
         this._tempRegistro =    this.FcnSQL.SelectDataRegistro( "param_anomalias",
                                                                 "lectura, mensaje, foto",
                                                                 "id_anomalia='"+this.anomalia+"'");
-        return _tempRegistro;
+        listarDatos.add(this._tempRegistro.getAsString("lectura"));
+        listarDatos.add(this._tempRegistro.getAsString("mensaje"));
+        listarDatos.add(this._tempRegistro.getAsString("foto"));
+        return listarDatos;
     }
 }
